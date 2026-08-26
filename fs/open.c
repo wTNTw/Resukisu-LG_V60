wTNTw/Resukisu-LG_V60

commit 90cc668df594bc3b2bc6fa2355f7684319a489ad

fs: adapt KSU sucompat hooks to struct filename API
ReSukiSU refactored sucompat (75b99e3, synced KernelSU PR #3521): under
CONFIG_KSU_SUSFS, ksu_handle_faccessat/ksu_handle_stat now take
`struct filename **` and rewrite filename->name directly.

The old callers passed `const char __user **`, so the kernel dereferenced
a userspace pointer in kernel mode (PAN fault, ESR 9600000f) -> Oops ->
panic during boot when ksud called faccessat().

- do_faccessat: getname() before the hook, use filename_lookup() when the
  hook ran (name->name points to kernel memory), putname() on all exits
- vfs_statx: same treatment for ksu_handle_stat
- non-sucompat fast path keeps user_path_at() and zero extra overhead

Fixes boot failure (Kernel panic: Fatal exception in ksu_handle_faccessat)
with ReSukiSU >= 75b99e3.

fs/open.c
@@ -372,8 +372,10 @@ SYSCALL_DEFINE4(fallocate, int, fd, int, mode, loff_t, offset, loff_t, len)
 #ifdef CONFIG_KSU_SUSFS
 extern struct static_key_true ksu_su_compat_enabled;
 extern bool __ksu_is_allow_uid_for_current(uid_t uid);
-extern int ksu_handle_faccessat(int *dfd, const char __user **filename_user, int *mode,
+extern int ksu_handle_faccessat(int *dfd, struct filename **filename, int *mode,
 			int *flags);
+extern int filename_lookup(int dfd, struct filename *name, unsigned flags,
+			struct path *path, struct path *root);
 #endif
 long do_faccessat(int dfd, const char __user *filename, int mode)
 {
@@ -382,6 +384,7 @@ long do_faccessat(int dfd, const char __user *filename, int mode)
 	struct path path;
 	struct inode *inode;
 	struct vfsmount *mnt;
+	struct filename *name = NULL;
 	int res;
 	unsigned int lookup_flags = LOOKUP_FOLLOW;
 
@@ -390,17 +393,20 @@ long do_faccessat(int dfd, const char __user *filename, int mode)
 		goto orig_flow;
 	if (static_branch_likely(&ksu_su_compat_enabled))
 		if (unlikely(__ksu_is_allow_uid_for_current(current_uid().val))) {
-			ksu_handle_faccessat(&dfd, &filename, &mode, NULL);
+			name = getname(filename);
+			if (IS_ERR(name))
+				return PTR_ERR(name);
+			ksu_handle_faccessat(&dfd, &name, &mode, NULL);
 	}
 
 orig_flow:
 #endif
 	if (mode & ~S_IRWXO)	/* where's F_OK, X_OK, W_OK, R_OK? */
-		return -EINVAL;
+		goto out_putname;
 
 	override_cred = prepare_creds();
 	if (!override_cred)
-		return -ENOMEM;
+		goto out_putname;
 
 	override_cred->fsuid = override_cred->uid;
 	override_cred->fsgid = override_cred->gid;
@@ -436,7 +442,10 @@ orig_flow:
 
 	old_cred = override_creds(override_cred);
 retry:
-	res = user_path_at(dfd, filename, lookup_flags, &path);
+	if (name)
+		res = filename_lookup(dfd, name, lookup_flags, &path, NULL);
+	else
+		res = user_path_at(dfd, filename, lookup_flags, &path);
 	if (res)
 		goto out;
 
@@ -479,6 +488,9 @@ out_path_release:
 out:
 	revert_creds(old_cred);
 	put_cred(override_cred);
+out_putname:
+	if (name)
+		putname(name);
 	return res;
 }
 

fs/stat.c
@@ -196,13 +196,16 @@ EXPORT_SYMBOL(vfs_statx_fd);
 #ifdef CONFIG_KSU_SUSFS
 extern struct static_key_true ksu_su_compat_enabled;
 extern bool __ksu_is_allow_uid_for_current(uid_t uid);
-extern int ksu_handle_stat(int *dfd, const char __user **filename_user, int *flags);
+extern int ksu_handle_stat(int *dfd, struct filename **filename, int *flags);
+extern int filename_lookup(int dfd, struct filename *name, unsigned flags,
+			struct path *path, struct path *root);
 #endif
 
 int vfs_statx(int dfd, const char __user *filename, int flags,
 	      struct kstat *stat, u32 request_mask)
 {
 	struct path path;
+	struct filename *name = NULL;
 	int error = -EINVAL;
 	unsigned int lookup_flags = LOOKUP_FOLLOW | LOOKUP_AUTOMOUNT;
 
@@ -210,15 +213,21 @@ int vfs_statx(int dfd, const char __user *filename, int flags,
 	if (likely(susfs_is_current_proc_umounted()))
 		goto orig_flow;
 	if (static_branch_likely(&ksu_su_compat_enabled)) {
-		if (unlikely(__ksu_is_allow_uid_for_current(current_uid().val)))
-			ksu_handle_stat(&dfd, &filename, &flags);
+		if (unlikely(__ksu_is_allow_uid_for_current(current_uid().val))) {
+			name = getname(filename);
+			if (IS_ERR(name))
+				return PTR_ERR(name);
+			ksu_handle_stat(&dfd, &name, &flags);
+		}
 	}
 orig_flow:
 #endif
 
 	if ((flags & ~(AT_SYMLINK_NOFOLLOW | AT_NO_AUTOMOUNT |
-		       AT_EMPTY_PATH | KSTAT_QUERY_FLAGS)) != 0)
-		return -EINVAL;
+		       AT_EMPTY_PATH | KSTAT_QUERY_FLAGS)) != 0) {
+		error = -EINVAL;
+		goto out_putname;
+	}
 
 	if (flags & AT_SYMLINK_NOFOLLOW)
 		lookup_flags &= ~LOOKUP_FOLLOW;
@@ -228,7 +237,10 @@ orig_flow:
 		lookup_flags |= LOOKUP_EMPTY;
 
 retry:
-	error = user_path_at(dfd, filename, lookup_flags, &path);
+	if (name)
+		error = filename_lookup(dfd, name, lookup_flags, &path, NULL);
+	else
+		error = user_path_at(dfd, filename, lookup_flags, &path);
 	if (error)
 		goto out;
 
@@ -239,6 +251,9 @@ retry:
 		goto retry;
 	}
 out:
+out_putname:
+	if (name)
+		putname(name);
 	return error;
 }
 EXPORT_SYMBOL(vfs_statx);

commit ff0607fa5b724bae237fb707d11672b109e830c5

fs: fix double putname in KSU sucompat hooks
filename_lookup() consumes the filename (it calls putname() internally),
so calling putname() again after it double-frees the object and hits
BUG_ON(name->refcnt <= 0) in fs/namei.c during boot (ksud faccessat).

- track sucompat hit with use_filename, getname() per lookup attempt
- null out name after filename_lookup() consumes it
- only putname() on early-exit paths (before lookup)

Fixes kernel BUG at fs/namei.c:266, regression from 90cc668df.

fs/open.c
@@ -387,17 +387,21 @@ long do_faccessat(int dfd, const char __user *filename, int mode)
 	struct filename *name = NULL;
 	int res;
 	unsigned int lookup_flags = LOOKUP_FOLLOW;
+#ifdef CONFIG_KSU_SUSFS
+	bool use_filename = false;
+#endif
 
 #ifdef CONFIG_KSU_SUSFS
 	if (likely(susfs_is_current_proc_umounted()))
 		goto orig_flow;
 	if (static_branch_likely(&ksu_su_compat_enabled))
 		if (unlikely(__ksu_is_allow_uid_for_current(current_uid().val))) {
+			use_filename = true;
 			name = getname(filename);
 			if (IS_ERR(name))
 				return PTR_ERR(name);
 			ksu_handle_faccessat(&dfd, &name, &mode, NULL);
-	}
+		}
 
 orig_flow:
 #endif
@@ -442,9 +446,20 @@ orig_flow:
 
 	old_cred = override_creds(override_cred);
 retry:
-	if (name)
+#ifdef CONFIG_KSU_SUSFS
+	if (use_filename) {
+		if (!name) {
+			name = getname(filename);
+			if (IS_ERR(name)) {
+				res = PTR_ERR(name);
+				name = NULL;
+				goto out;
+			}
+		}
 		res = filename_lookup(dfd, name, lookup_flags, &path, NULL);
-	else
+		name = NULL;
+	} else
+#endif
 		res = user_path_at(dfd, filename, lookup_flags, &path);
 	if (res)
 		goto out;

fs/stat.c
@@ -208,12 +208,16 @@ int vfs_statx(int dfd, const char __user *filename, int flags,
 	struct filename *name = NULL;
 	int error = -EINVAL;
 	unsigned int lookup_flags = LOOKUP_FOLLOW | LOOKUP_AUTOMOUNT;
+#ifdef CONFIG_KSU_SUSFS
+	bool use_filename = false;
+#endif
 
 #ifdef CONFIG_KSU_SUSFS
 	if (likely(susfs_is_current_proc_umounted()))
 		goto orig_flow;
 	if (static_branch_likely(&ksu_su_compat_enabled)) {
 		if (unlikely(__ksu_is_allow_uid_for_current(current_uid().val))) {
+			use_filename = true;
 			name = getname(filename);
 			if (IS_ERR(name))
 				return PTR_ERR(name);
@@ -237,9 +241,20 @@ orig_flow:
 		lookup_flags |= LOOKUP_EMPTY;
 
 retry:
-	if (name)
+#ifdef CONFIG_KSU_SUSFS
+	if (use_filename) {
+		if (!name) {
+			name = getname(filename);
+			if (IS_ERR(name)) {
+				error = PTR_ERR(name);
+				name = NULL;
+				goto out;
+			}
+		}
 		error = filename_lookup(dfd, name, lookup_flags, &path, NULL);
-	else
+		name = NULL;
+	} else
+#endif
 		error = user_path_at(dfd, filename, lookup_flags, &path);
 	if (error)
 		goto out;